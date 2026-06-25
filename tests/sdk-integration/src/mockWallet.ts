1ad973e8f41348080e74ea78dc89f147e5517ebb
Alimzy <salamialameen20@gmail.com>
2026-06-25 09:39:54 +0100

    chore(tests): add SDK integration testing helpers

---
+++ tests/sdk-integration/src/mockWallet.ts
@@ -0,0 +1,40 @@
+import { EventEmitter } from 'events';
+
+export interface MockWalletOptions {
+  address?: string;
+}
+
+export class MockWallet extends EventEmitter {
+  address: string;
+
+  constructor(opts: MockWalletOptions = {}) {
+    super();
+    this.address = opts.address ?? 'GMOCKWALLETADDRESS000000000000000000000';
+  }
+
+  async connect() {
+    this.emit('connect', { address: this.address });
+    return { address: this.address };
+  }
+
+  async disconnect() {
+    this.emit('disconnect');
+  }
+
+  async signTransaction(tx: unknown) {
+    // Return a lightweight mock signature structure
+    return {
+      tx,
+      signature: 'MOCK_SIGNATURE',
+      signer: this.address,
+    };
+  }
+
+  async signMessage(message: string) {
+    return {
+      message,
+      signature: 'MOCK_MESSAGE_SIGNATURE',
+      signer: this.address,
+    };
+  }
+}
